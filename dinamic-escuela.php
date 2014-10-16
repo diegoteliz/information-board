fbb0cfa2236d5a0bb19762b46cf9db7b11bfa02c
Diego <diegoteliz007@gmail.com>
2014-10-16 19:21:55 -0200

    update

--- dinamic-escuela.php
+++ dinamic-escuela.php
@@ -13,84 +13,146 @@ include_once('class/class.billboard.php');  ?>
 							<h4>Información no disponible</h4>
 						</li> -->
 						<li class="list-item emt-item">
-							<h4>Semana del 14 de Julio</h4>
-
-							<div class="sala">Sala Principal</div>
-							<div class="asignacion"><b>Puntos sobresalientes:</b> Michel Fuentes</div>
-							<div class="info">Levítico 21 a 24</div>
-							<div class="asignacion"><b>Asignación Nº1:</b> Ariel Fuentes</div>
-							<div class="info">Levítico 23:1-14</div>
-							<div class="asignacion"><b>Asignación Nº2:</b> Fanny Rodríguez	- Fabiana de Demarco</div>
-							<div class="info">La Biblia no enseña que todos los seres humanos se salvarán (rs pág. 340 párr. 3)</div>
-							<div class="asignacion"><b>Asignación Nº3:</b> Humberto Cardozo</div>
-							<div class="info">Tiempo acepto. Aproveche sabiamente el período favorable que Dios concede (it-2 págs. 1115, 1116)</div>
-
-							<div class="sala">Sala Auxiliar</div>
-							<div class="asignacion"><b>Asignación Nº1:</b> Geremías Fuentes</div>
-							<div class="info">Levítico 23:1-14</div>
-							<div class="asignacion"><b>Asignación Nº2:</b> Carolina Giménez - Liana Sosa</div>
-							<div class="info">La Biblia no enseña que todos los seres humanos se salvarán (rs pág. 340 párr. 3)</div>
-							<div class="asignacion"><b>Asignación Nº3:</b> Rubens Rivero</div>
-							<div class="info">Tiempo acepto. Aproveche sabiamente el período favorable que Dios concede (it-2 págs. 1115, 1116)</div>
-						</li>
-
-						<li class="list-item emt-item">
-							<h4>Semana del 21 de Julio</h4>
-
-							<div class="sala">Sala Principal (Semana de la visita del superintendente)</div>
-							<div class="asignacion"><b>Puntos sobresalientes:</b> Germán Demarco</div>
-							<div class="info">Levítico 25 a 27</div>
-							<div class="asignacion"><b>Asignación Nº1:</b> Marcelo Paolino</div>
-							<div class="info">Levítico 26:1-17</div>
-							<div class="asignacion"><b>Asignación Nº2:</b> Miriam Fontes - Katherine Cabrera</div>
-							<div class="info">¿Acabarán salvándose todos los seres humanos? (rs pág. 341 párr. 1)</div>
-							<div class="asignacion"><b>Asignación Nº3:</b> Diego Téliz</div>
-							<div class="info">Accidente, Accidental. La Biblia distingue entre sucesos accidentales e intencionales (it-1 pág. 41–pág. 42 párr. 2)</div>
-						</li>
-
-						<li class="list-item emt-item">
-							<h4>Semana del 28 de Julio</h4>
-
-							<div class="sala">Sala Principal</div>
-							<div class="asignacion"><b>Puntos sobresalientes:</b> Rubens Rivero</div>
-							<div class="info">Números 1 a 3</div>
-							<div class="asignacion"><b>Asignación Nº1:</b> Geremías Fuentes</div>
-							<div class="info">Números 3:21-38</div>
-							<div class="asignacion"><b>Asignación Nº2:</b> Susana Echeveste - Natalia de Paolino</div>
-							<div class="info">“Toda clase de hombres” se salvará (rs pág. 341 párr. 2)</div>
-							<div class="asignacion"><b>Asignación Nº3:</b> Ricardo Rodríguez</div>
-							<div class="info">Acusación. ¿Cómo se atendían las acusaciones bajo las leyes hebrea y romana? (it-1 pág. 46–pág. 47 párr. 1)</div>
-
-							<div class="sala">Sala Auxiliar</div>
-							<div class="asignacion"><b>Asignación Nº1:</b> Humberto Cardozo</div>
-							<div class="info">Números 3:21-38</div>
-							<div class="asignacion"><b>Asignación Nº2:</b> Mónica Estrada - Mirta de Navarro</div>
-							<div class="info">“Toda clase de hombres” se salvará (rs pág. 341 párr. 2)</div>
-							<div class="asignacion"><b>Asignación Nº3:</b> Germán Demarco</div>
-							<div class="info">Acusación. ¿Cómo se atendían las acusaciones bajo las leyes hebrea y romana? (it-1 pág. 46–pág. 47 párr. 1)</div>
-						</li>
-
-						<li class="list-item emt-item">
-							<h4>Semana del 4 de Agosto</h4>
+							<h4>Semana del 13 de Octubre</h4>
 
 							<div class="sala">Sala Principal</div>
 							<div class="asignacion"><b>Puntos sobresalientes:</b> Iván Rivero</div>
-							<div class="info">Números 4 a 6</div>
+							<div class="info">Deuteronomio 4 a 6</div>
+							<div class="asignacion"><b>Asignación Nº1:</b> Geremías Fuentes</div>
+							<div class="info">Deuteronomio 4:29-43</div>
+							<div class="asignacion"><b>Asignación Nº2:</b> Leticia Rodríguez - Yanela de Rivero</div>
+							<div class="info">Cambios que debemos hacer para agradar a Dios (rs pág. 363 párrs. 1-3)</div>
+							<div class="asignacion"><b>Asignación Nº3:</b> Osvaldo Simone</div>
+							<div class="info">¿Qué dice la Biblia sobre la homosexualidad? (rs pág. 362 párrs. 3-5)</div>
+
+							<div class="sala">Sala Auxiliar</div>
+							<div class="asignacion"><b>Asignación Nº1:</b> Diego Téliz</div>
+							<div class="info">Deuteronomio 4:29-43</div>
+							<div class="asignacion"><b>Asignación Nº2:</b> Liana Sosa - Daycir Viana</div>
+							<div class="info">Cambios que debemos hacer para agradar a Dios (rs pág. 363 párrs. 1-3)</div>
+							<div class="asignacion"><b>Asignación Nº3:</b> Ariel Fuentes</div>
+							<div class="info">¿Qué dice la Biblia sobre la homosexualidad? (rs pág. 362 párrs. 3-5)</div>
+						</li>
+
+						<li class="list-item emt-item">
+							<h4>Semana del 20 de Octubre</h4>
+
+							<div class="sala">Sala Principal</div>
+							<div class="asignacion"><b>Puntos sobresalientes:</b> Germán Demarco</div>
+							<div class="info">Deuteronomio 7 a 10</div>
 							<div class="asignacion"><b>Asignación Nº1:</b> Erick Garat</div>
-							<div class="info">Números 4:17-33</div>
-							<div class="asignacion"><b>Asignación Nº2:</b> Giannina Paolino - Leticia Rodríguez</div>
-							<div class="info">¿Enseña la Biblia que algunas personas nunca se salvarán? (rs pág. 342 párrs. 1-3)</div>
-							<div class="asignacion"><b>Asignación Nº3:</b> Rubens Rivero</div>
-							<div class="info">Acusación. Jehová está por encima de las leyes injustas y juzga a quienes hacen acusaciones falsas (it-1 pág. 47 párrs. 2, 3)</div>
+							<div class="info">Deuteronomio 9:15-29</div>
+							<div class="asignacion"><b>Asignación Nº2:</b> Nélida de los Santos - Vanessa Cabrera</div>
+							<div class="info">¿Cómo pudo pecar un hombre perfecto? (rs pág. 282 párrs. 1-4)</div>
+							<div class="asignacion"><b>Asignación Nº3:</b> Alexa Rodríguez - Zully Campelo</div>
+							<div class="info">Adonías. No se oponga a las decisiones de Jehová (it-1 pág. 55, Adonías núm. 1)</div>
+
+							<div class="sala">Sala Auxiliar</div>
+							<div class="asignacion"><b>Asignación Nº1:</b> Humberto Cardozo</div>
+							<div class="info">Deuteronomio 9:15-29</div>
+							<div class="asignacion"><b>Asignación Nº2:</b> Giannina Paolino - Fiorella Rodríguez</div>
+							<div class="info">¿Cómo pudo pecar un hombre perfecto? (rs pág. 282 párrs. 1-4)</div>
+							<div class="asignacion"><b>Asignación Nº3:</b> Luci de Rivero - Teresa de Figueredo</div>
+							<div class="info">Adonías. No se oponga a las decisiones de Jehová (it-1 pág. 55, Adonías núm. 1)</div>
+						</li>
+
+						<li class="list-item emt-item">
+							<h4>Semana del 27 de Octubre</h4>
+
+							<div class="sala">Sala Principal</div>
+							<div class="asignacion"><b>Puntos sobresalientes:</b> Ricardo Rodríguez</div>
+							<div class="info">Deuteronomio 11 a 13</div>
+							<div class="asignacion">Repaso Oral</div>
+						</li>
+
+						<li class="list-item emt-item">
+							<h4>Semana del 3 de Noviembre</h4>
+
+							<div class="sala">Sala Principal</div>
+							<div class="asignacion"><b>Puntos sobresalientes:</b> Iván Rivero</div>
+							<div class="info">Deuteronomio 14 a 18</div>
+							<div class="asignacion"><b>Asignación Nº1:</b> Ariel Fuentes</div>
+							<div class="info">Deuteronomio 15:16–16:8</div>
+							<div class="asignacion"><b>Asignación Nº2:</b> Zully Campelo - Lilian López</div>
+							<div class="info">¿Por qué no le restamos gravedad al pecado? (rs pág. 283 párr. 2–pág. 284 párr. 3)</div>
+							<div class="asignacion"><b>Asignación Nº3:</b> Jael Oviedo - Claudia de Guevara</div>
+							<div class="info">Adopción. Cómo se veía y practicaba la adopción en tiempos de las Escrituras Hebreas (it-1 pág. 56 párrs. 1-4)</div>
+
+							<div class="sala">Sala Auxiliar</div>
+							<div class="asignacion"><b>Asignación Nº1:</b> Marcelo Paolino</div>
+							<div class="info">Deuteronomio 15:16–16:8</div>
+							<div class="asignacion"><b>Asignación Nº2:</b> Analía Rodríguez - Angélica Sensever</div>
+							<div class="info">¿Por qué no le restamos gravedad al pecado? (rs pág. 283 párr. 2–pág. 284 párr. 3)</div>
+							<div class="asignacion"><b>Asignación Nº3:</b> Natalia de Paolino - Blanca Pereira</div>
+							<div class="info">Adopción. Cómo se veía y practicaba la adopción en tiempos de las Escrituras Hebreas (it-1 pág. 56 párrs. 1-4)</div>
+						</li>
+
+						<li class="list-item emt-item">
+							<h4>Semana del 10 de Noviembre</h4>
+
+							<div class="sala">Sala Principal</div>
+							<div class="asignacion"><b>Puntos sobresalientes:</b> -</div>
+							<div class="info">Deuteronomio 19 a 22</div>
+							<div class="asignacion"><b>Asignación Nº1:</b> Diego Téliz</div>
+							<div class="info">Deuteronomio 22:20-30</div>
+							<div class="asignacion"><b>Asignación Nº2:</b> Esther de Simone - Isabel Rivero</div>
+							<div class="info">El efecto del pecado en nuestra relación con Dios (rs pág. 285 párrs. 1-4)</div>
+							<div class="asignacion"><b>Asignación Nº3:</b> Jennifer de Téliz - Fabiana de Demarco</div>
+							<div class="info">Adopción. Jehová adopta hijos espirituales mediante Cristo Jesús (it-1 pág. 56 párrs. 5-7)</div>
+
+							<div class="sala">Sala Auxiliar</div>
+							<div class="asignacion"><b>Asignación Nº1:</b> Osvaldo Simone</div>
+							<div class="info">Deuteronomio 22:20-30</div>
+							<div class="asignacion"><b>Asignación Nº2:</b> Rosario Telechea - Carolina Giménez</div>
+							<div class="info">El efecto del pecado en nuestra relación con Dios (rs pág. 285 párrs. 1-4)</div>
+							<div class="asignacion"><b>Asignación Nº3:</b> Vanessa Cabrera - Rossana de Sandes</div>
+							<div class="info">Adopción. Jehová adopta hijos espirituales mediante Cristo Jesús (it-1 pág. 56 párrs. 5-7)</div>
+						</li>
+
+						<li class="list-item emt-item">
+							<h4>Semana del 17 de Noviembre</h4>
+
+							<div class="sala">Sala Principal</div>
+							<div class="asignacion"><b>Puntos sobresalientes:</b> -</div>
+							<div class="info">Deuteronomio 23 a 27</div>
+							<div class="asignacion"><b>Asignación Nº1:</b> Geremías Fuentes</div>
+							<div class="info">Deuteronomio 25:17–26:10</div>
+							<div class="asignacion"><b>Asignación Nº2:</b> Daycir Viana - Mirtha de Navarro</div>
+							<div class="info">¿Qué dice la Biblia sobre el alma? (rs pág. 32 párr. 2–pág. 33 párr. 3)</div>
+							<div class="asignacion"><b>Asignación Nº3:</b> Micaela Sandes - Alexa Rodríguez</div>
+							<div class="info">Adorno(s). Los adornos en tiempos bíblicos (it-1 pág. 58 párr. 3–pág. 59 párr. 3)</div>
+
+							<div class="sala">Sala Auxiliar</div>
+							<div class="asignacion"><b>Asignación Nº1:</b> Erick Garat</div>
+							<div class="info">Deuteronomio 25:17–26:10</div>
+							<div class="asignacion"><b>Asignación Nº2:</b> Katherine Cabrera - Blanca Pereira</div>
+							<div class="info">¿Qué dice la Biblia sobre el alma? (rs pág. 32 párr. 2–pág. 33 párr. 3)</div>
+							<div class="asignacion"><b>Asignación Nº3:</b> Humberto Cardozo</div>
+							<div class="info">Adorno(s). Los adornos en tiempos bíblicos (it-1 pág. 58 párr. 3–pág. 59 párr. 3)</div>
+						</li>
+
+						<li class="list-item emt-item">
+							<h4>Semana del 24 de Noviembre</h4>
+
+							<div class="sala">Sala Principal</div>
+							<div class="asignacion"><b>Puntos sobresalientes:</b> -</div>
+							<div class="info">Deuteronomio 28 a 31</div>
+							<div class="asignacion"><b>Asignación Nº1:</b> Humberto Cardozo</div>
+							<div class="info">Deuteronomio 30:15–31:8</div>
+							<div class="asignacion"><b>Asignación Nº2:</b> Miriam Fontes - Angélica Sensever</div>
+							<div class="info">Los animales son almas (rs pág. 33 párr. 4–pág. 34 párr. 3)</div>
+							<div class="asignacion"><b>Asignación Nº3:</b> Geremías Fuentes</div>
+							<div class="info">Adorno(s). Consejos cristianos sobre el adorno personal (it-1 pág. 59 párrs. 4, 5)</div>
 
 							<div class="sala">Sala Auxiliar</div>
 							<div class="asignacion"><b>Asignación Nº1:</b> Ariel Fuentes</div>
-							<div class="info">Números 4:17-33</div>
-							<div class="asignacion"><b>Asignación Nº2:</b> Jennifer de Téliz - Rossana de Sandes</div>
-							<div class="info">¿Enseña la Biblia que algunas personas nunca se salvarán? (rs pág. 342 párrs. 1-3)</div>
-							<div class="asignacion"><b>Asignación Nº3:</b> Marcelo Paolino</div>
-							<div class="info">Acusación. Jehová está por encima de las leyes injustas y juzga a quienes hacen acusaciones falsas (it-1 pág. 47 párrs. 2, 3)</div>
+							<div class="info">Deuteronomio 30:15–31:8</div>
+							<div class="asignacion"><b>Asignación Nº2:</b> Fabiana de Demarco - Mónica Estrada</div>
+							<div class="info">Los animales son almas (rs pág. 33 párr. 4–pág. 34 párr. 3)</div>
+							<div class="asignacion"><b>Asignación Nº3:</b> Shirley de Cabrera - Nélida de los Santos</div>
+							<div class="info">Adorno(s). Consejos cristianos sobre el adorno personal (it-1 pág. 59 párrs. 4, 5)</div>
 						</li>
+
 					</ul>
 				</div>
 			</section>
